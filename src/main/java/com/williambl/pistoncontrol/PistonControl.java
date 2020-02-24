4ae04b978f5a48e6ab896bce83eb738ca1435853
williambl <william@williambl.com>
2020-02-24 17:20:41 +0000

    Add mixin to block piston behaviour check

--- src/main/java/com/williambl/pistoncontrol/PistonControl.java
+++ src/main/java/com/williambl/pistoncontrol/PistonControl.java
@@ -1,8 +1,16 @@
 package com.williambl.pistoncontrol;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.tag.Tag;
 
 public class PistonControl implements ModInitializer {
+
+	public static Tag<Block> PISTON_BEHAVIOR_NORMAL;
+	public static Tag<Block> PISTON_BEHAVIOR_DESTROY;
+	public static Tag<Block> PISTON_BEHAVIOR_BLOCK;
+	public static Tag<Block> PISTON_BEHAVIOR_IGNORE;
+	public static Tag<Block> PISTON_BEHAVIOR_PUSH_ONLY;
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/com/williambl/pistoncontrol/mixin/BlockMixin.java
@@ -0,0 +1,31 @@
+package com.williambl.pistoncontrol.mixin;
+
+import com.williambl.pistoncontrol.PistonControl;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.piston.PistonBehavior;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Block.class)
+public class BlockMixin {
+    @Inject(
+            at = @At("HEAD"),
+            method = "getPistonBehavior(Lnet.minecraft.block.BlockState)Lnet.minecraft.block.piston.PistonBehavior",
+            cancellable = true
+    )
+    private void getPistonBehavior(BlockState state, CallbackInfoReturnable<PistonBehavior> info) {
+        if (PistonControl.PISTON_BEHAVIOR_NORMAL.contains(state.getBlock()))
+            info.setReturnValue(PistonBehavior.NORMAL);
+        if (PistonControl.PISTON_BEHAVIOR_DESTROY.contains(state.getBlock()))
+            info.setReturnValue(PistonBehavior.DESTROY);
+        if (PistonControl.PISTON_BEHAVIOR_BLOCK.contains(state.getBlock()))
+            info.setReturnValue(PistonBehavior.BLOCK);
+        if (PistonControl.PISTON_BEHAVIOR_IGNORE.contains(state.getBlock()))
+            info.setReturnValue(PistonBehavior.IGNORE);
+        if (PistonControl.PISTON_BEHAVIOR_PUSH_ONLY.contains(state.getBlock()))
+            info.setReturnValue(PistonBehavior.PUSH_ONLY);
+    }
+}
