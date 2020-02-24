ecb13567c0f61198a82747b1abb6af66225521f3
williambl <william@williambl.com>
2020-02-24 16:08:00 +0000

    Set up mod

---
+++ src/main/java/com/williambl/pistoncontrol/PistonControl.java
@@ -0,0 +1,10 @@
+package com.williambl.pistoncontrol;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PistonControl implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
