2430edc596f686001335335eba7719591981cf7e
williambl <william@williambl.com>
2020-02-25 17:26:01 +0000

    Add initialisation of tags

--- src/main/java/com/williambl/pistoncontrol/PistonControl.java
+++ src/main/java/com/williambl/pistoncontrol/PistonControl.java
@@ -1,16 +1,20 @@
 package com.williambl.pistoncontrol;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
 
 public class PistonControl implements ModInitializer {
 
-	public static Tag<Block> PISTON_BEHAVIOR_NORMAL;
-	public static Tag<Block> PISTON_BEHAVIOR_DESTROY;
-	public static Tag<Block> PISTON_BEHAVIOR_BLOCK;
-	public static Tag<Block> PISTON_BEHAVIOR_IGNORE;
-	public static Tag<Block> PISTON_BEHAVIOR_PUSH_ONLY;
+	private static String MODID = "pistoncontrol";
+
+	public static Tag<Block> PISTON_BEHAVIOR_NORMAL = TagRegistry.block(new Identifier(MODID, "piston_behavior_normal"));
+	public static Tag<Block> PISTON_BEHAVIOR_DESTROY = TagRegistry.block(new Identifier(MODID, "piston_behavior_destroy"));
+	public static Tag<Block> PISTON_BEHAVIOR_BLOCK = TagRegistry.block(new Identifier(MODID, "piston_behavior_block"));
+	public static Tag<Block> PISTON_BEHAVIOR_IGNORE = TagRegistry.block(new Identifier(MODID, "piston_behavior_ignore"));
+	public static Tag<Block> PISTON_BEHAVIOR_PUSH_ONLY = TagRegistry.block(new Identifier(MODID, "piston_behavior_push_only"));
 
 	@Override
 	public void onInitialize() {
