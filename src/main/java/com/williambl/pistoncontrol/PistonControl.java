f95a045ca9f659e2a2c41388ee4b79a2d2a73965
williambl <william@williambl.com>
2020-02-28 22:06:10 +0000

    Add a method to move BEs

--- src/main/java/com/williambl/pistoncontrol/PistonControl.java
+++ src/main/java/com/williambl/pistoncontrol/PistonControl.java
@@ -3,8 +3,15 @@ package com.williambl.pistoncontrol;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 
 public class PistonControl implements ModInitializer {
 
@@ -19,5 +26,25 @@ public class PistonControl implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+	}
+
+	public void moveBE(World world, BlockPos pos, Direction direction) {
+		BlockPos newPos = pos.offset(direction.getOpposite());
+		BlockEntity be = world.getBlockEntity(pos);
+		BlockState bs = world.getBlockState(pos);
+		if (be == null)
+			return;
+		if (world.isClient) {
+			world.updateListeners(newPos, bs, bs, 2);
+			return;
+		}
+		be.setPos(newPos);
+		CompoundTag tag = new CompoundTag();
+		be.toTag(tag);
+		world.removeBlockEntity(pos);
+		world.removeBlock(pos, true);
+		world.setBlockState(newPos, bs);
+		world.getBlockEntity(newPos).fromTag(tag);
+		((ServerWorld) world).getChunkManager().markForUpdate(newPos);
 	}
 }
