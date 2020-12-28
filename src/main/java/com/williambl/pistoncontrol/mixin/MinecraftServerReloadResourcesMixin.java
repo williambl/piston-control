3892b99c2bf9f22ef84eb686e9da1211fb8249d6
williambl <william@williambl.com>
2020-12-28 19:10:05 +0000

    Replace IterableHashSet and IterableBlockTagMap
    with an intersect method operating on regular collections
    and a guava multimap, respectively

--- src/main/java/com/williambl/pistoncontrol/mixin/MinecraftServerReloadResourcesMixin.java
+++ src/main/java/com/williambl/pistoncontrol/mixin/MinecraftServerReloadResourcesMixin.java
@@ -1,6 +1,5 @@
 package com.williambl.pistoncontrol.mixin;
 
-import com.williambl.pistoncontrol.IterableHashSet;
 import net.minecraft.block.Block;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tag.BlockTags;
@@ -12,8 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
 import static com.williambl.pistoncontrol.PistonControl.STICKY_MAP;
@@ -37,8 +35,7 @@ public abstract class MinecraftServerReloadResourcesMixin {
         tags.keySet().stream().filter(it -> it.getPath().startsWith("selectively_sticky"))
                 .forEach(id -> {
                     Tag<Block> tag = tags.get(id);
-                    IterableHashSet<Tag<Block>> tagSet = new IterableHashSet<>(tag);
-                    tag.values().forEach(block -> STICKY_MAP.put(block, tagSet));
+                    tag.values().forEach(block -> STICKY_MAP.put(block, tag));
                 });
     }
 }
