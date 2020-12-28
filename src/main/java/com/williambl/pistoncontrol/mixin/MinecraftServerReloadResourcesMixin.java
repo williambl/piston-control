d93f1fb25b87ca3b6d8e6d1dfe4952f223af85a5
williambl <william@williambl.com>
2020-12-28 15:06:12 +0000

    Add discovery of sticky-channel tags

--- src/main/java/com/williambl/pistoncontrol/mixin/MinecraftServerReloadResourcesMixin.java
+++ src/main/java/com/williambl/pistoncontrol/mixin/MinecraftServerReloadResourcesMixin.java
@@ -1,31 +1,22 @@
 package com.williambl.pistoncontrol.mixin;
 
 import com.williambl.pistoncontrol.IterableHashSet;
-import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.resource.ResourcePackManager;
-import net.minecraft.resource.ResourcePackProfile;
-import net.minecraft.resource.ResourceType;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import static com.williambl.pistoncontrol.PistonControl.STICKY_MAP;
-import static com.williambl.pistoncontrol.PistonControl.getMODID;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerReloadResourcesMixin {
@@ -42,27 +33,12 @@ public abstract class MinecraftServerReloadResourcesMixin {
     private void setSelectivelySticky() {
         STICKY_MAP.clear();
 
-        for (Block plank : BlockTags.PLANKS.values())
-          STICKY_MAP.put(plank, new IterableHashSet<>(BlockTags.PLANKS));
-
-        for (Block log : BlockTags.LOGS.values())
-            STICKY_MAP.put(log, new IterableHashSet<>(BlockTags.LOGS));
-
-//        ArrayList<ResourcePackProfile> profiles = new ArrayList<>(getDataPackManager().getProfiles());
-//
-//        for (ResourcePackProfile profile : profiles) {
-//            Collection<Identifier> foundResources = profile.createResourcePack().findResources(ResourceType.SERVER_DATA, getMODID(), "tags/blocks/selectively_sticky", Integer.MAX_VALUE, (string) -> true);
-//            if (!foundResources.isEmpty()) {
-//                for (Identifier id : foundResources) {
-//                    Tag<Block> blockTag = TagRegistry.block(id);
-//                    List<Block> tagValues = blockTag.values();
-//                    for (Block block : tagValues)
-//                        STICKY_MAP.put(block, new IterableHashSet<>(blockTag));
-//                }
-//            }
-//        }
+        Map<Identifier, Tag<Block>> tags = BlockTags.getContainer().getEntries();
+        tags.keySet().stream().filter(it -> it.getPath().startsWith("selectively_sticky"))
+                .forEach(id -> {
+                    Tag<Block> tag = tags.get(id);
+                    IterableHashSet<Tag<Block>> tagSet = new IterableHashSet<>(tag);
+                    tag.values().forEach(block -> STICKY_MAP.put(block, tagSet));
+                });
     }
-
-    @Accessor abstract LevelStorage.Session getSession();
-    @Accessor abstract ResourcePackManager<ResourcePackProfile> getDataPackManager();
 }
